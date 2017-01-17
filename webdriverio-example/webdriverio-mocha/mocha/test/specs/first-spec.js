f66e038eb2c50567ed51adb206baf3d9bb534030
shiv <shivanand.swamy.ext@bbc.co.uk>
2017-01-17 18:47:04 +0000

    removed unwanted projects

--- webdriverio-example/webdriverio-mocha/mocha/test/specs/first-spec.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert')
-
-describe('webdriverio page', function() {
-  it('should have the right title', function() {
-    browser.url('/')
-    var title = browser.getTitle()
-    assert.equal(title, 'WebdriverIO - Selenium 2.0 javascript bindings for nodejs')
-  })
-})
